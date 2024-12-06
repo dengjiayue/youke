03f9798f48d934cff2545fe47862c37162121517
dengjiayue <3508225629@qq.com>
2024-12-06 12:18:03 +0800

    commit

--- model/init_model.go
+++ model/init_model.go
@@ -1,20 +1,17 @@
 package model
 
 import (
-	model_customer "youke/model/customer"
-	model_order "youke/model/order"
-
 	"gorm.io/gorm"
 )
 
 func InitDataBaseModel(db *gorm.DB) {
-	err := model_customer.CreateTable(db)
-	if err != nil {
-		panic(err)
-	}
-	err = model_order.CreateTable(db)
-	if err != nil {
-		panic(err)
-	}
+	// err := model_customer.CreateTable(db)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// err = model_order.CreateTable(db)
+	// if err != nil {
+	// 	panic(err)
+	// }
 	// ...
 }
